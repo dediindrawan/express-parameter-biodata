5dda0ce2122a8f6a8648003ac021e99b5197a6b2
Dedi Indrawan <dediindrawan812@gmail.com>
2023-05-11 23:30:01 +0700

    first commit

---
+++ biodata.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/biodata', function (req, res) {
+    const nama = req.query.nama;
+    const tempatLahir = req.query['tempat-lahir'];
+    const tanggalLahir = req.query['tanggal-lahir'];
+    const alamat = req.query.alamat;
+
+    res.send({
+        'nama': nama,
+        'tempat-lahir': tempatLahir,
+        'tanggal-lahir': tanggalLahir,
+        'alamat': alamat
+    });
+});
+
+app.post('/biodata', function (req, res) {
+    const nama = req.body.nama;
+    const tempatLahir = req.body['tempat-lahir'];
+    const tanggalLahir = req.body['tanggal-lahir'];
+    const alamat = req.body.alamat;
+
+    res.send({
+        'nama': nama,
+        'tempat-lahir': tempatLahir,
+        'tanggal-lahir': tanggalLahir,
+        'alamat': alamat
+    });
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+// http://localhost:8080/biodata?nama=Dedi%20Indrawan&tempat-lahir=Jakarta&tanggal-lahir=08121988&alamat=Jl.Griya%20Citayam%20Asri%20No.90
